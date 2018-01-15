e9ab71c20f9b94a18ea6261246bd5af1147188e5
ganesanns <ganesan.ns@cognizant.com>
2018-01-15 12:28:53 +0530

    Consume the message and send the message

---
+++ src/main/java/com/cts/pivotaltracker/KeyStore.java
@@ -0,0 +1,10 @@
+package com.cts.pivotaltracker;
+
+public class KeyStore {
+
+    public static final String APP_ID = "e0534308-c57c-47e1-94f5-07d650c97b53";
+    public static final String APP_SECRET = "7dd9c17e-4895-493a-8422-0178c5c59204";
+    public static final String BOT_USER_ID = "u:Bao2f5rndo2a6f55";
+    public static final String BOT_TOKEN = "3cd5f586-9890-4fdc-ad99-2bf1d00f6819";
+    public static UserInfo UserInfo = new UserInfo("a2202bee-2609-42a9-8314-6933fd5d0ad4", "u:sfvud8h39fuufvhv");
+}
---
+++ src/main/java/com/cts/pivotaltracker/UserInfo.java
@@ -0,0 +1,49 @@
+
+package com.cts.pivotaltracker;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UserInfo {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("userId")
+    @Expose
+    private String userId;
+
+    public UserInfo(String token, String userId) {
+
+        this.token = token;
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+}
